a9d767b89acd91c8ed55d949030493ec6167141a
“Jassidak <jassidaksingh@gmail.com>
2024-11-02 01:14:24 -0600

    fixed the 3d model

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -5,7 +5,7 @@ import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 import CanvasLoader from "../Loader";
 
 const Computers = ({ isMobile }) => {
-  const computer = useGLTF("./desktop_pc1/scene.gltf");
+  const computer = useGLTF("/scene.glb");
 
   return (
     <mesh>
